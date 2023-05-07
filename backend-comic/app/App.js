0e6ba4ae65cae36936bbca8e24c73637c817af96
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2023-05-07 16:05:04 +0700

    setup backend

--- backend-comic/app/App.js
+++ backend-comic/app/App.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
+const viewEngine = require("../config/viewEngine");
+const initWebRoutes = require("./Http/routes/Web");
+const bodyParser = require("body-parser");
+const initAPIRoutes = require("./Http/routes/Api");
+require("dotenv").config();
+//
 const app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+//
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+viewEngine(app);
+initWebRoutes(app);
+initAPIRoutes(app);
+let port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("connecting back to port " + port);
 });
